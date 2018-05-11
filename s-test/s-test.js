8d6e7e941c5d5d0b7af1a752f63a5b4b0c2a0322
Sascha Szirnyi <39184493+szirnyi@users.noreply.github.com>
2018-05-11 11:58:38 +0200

    Begrüssung

--- s-test/s-test.js
+++ s-test/s-test.js
@@ -15,18 +15,48 @@ class STest extends PolymerElement {
         :host {
           display: block;
         }
+        .container {
+          background-color: silver;
+          padding: 24px;
+        }
       </style>
-      <h2>Ciao, [[prop1]]!</h2>
+      <div class="container">
+        <h2>[[greeting]]</h2>
+      </div>
     `;
   }
   static get properties() {
     return {
-      prop1: {
+      username: {
         type: String,
-        value: 's-test',
+        value: '',
+        observer: '_doUsername'
       },
     };
   }
+
+  // ---------------------------------------------------------
+  // Functions
+  // ---------------------------------------------------------
+  
+  _doUsername() {
+    // Init
+    var username = this.get('username'); 
+
+    // Begrüssungstext festlegen
+    var greeting = 'Hoi' ;
+    if (username !== '') {
+      greeting = greeting + ', ' + username;  
+    };
+    greeting = greeting + '!';
+
+    // Variable - SET
+    this.set('greeting', greeting);
+     
+    // Prüfen
+    console.log('_doUsername', this.greeting);
+  }
+
 }
 
 window.customElements.define('s-test', STest);
